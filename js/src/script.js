839ee8ccf17b308ea4ee7004a8cd117ab31b32cf
VladDev26 <volodimirb23@gmail.com>
2016-06-16 18:48:03 +0300

    f

--- js/src/script.js
+++ js/src/script.js
@@ -20,6 +20,7 @@
 	xhr.open('GET', 'https://pixabay.com/api/?key=2696808-a99aca4232c52551e38c21475&image_type=photo', true);
 
 	xhr.onreadystatechange = function(){
+		var data = JSON.parse(this.responseText);
 		console.log('onreadystatechange');
 		console.log(data);
 	}
